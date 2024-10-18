81b56a83209ee3033ef141f1260b193ff734b3c6
Yassine EL OUAFI <yelouafi@gmail.com>
2024-10-18 15:03:56 +0100

    fix type errors

--- src/rooms/index.ts
+++ src/rooms/index.ts
@@ -1,7 +1,5 @@
 import { ColyseusGameRoom } from "./ColyseusGameRoom";
-import { TestRoom } from "./TestRoom";
 
 export const rooms = {
-  "test-room": TestRoom,
   "cyber-game": ColyseusGameRoom,
 };
